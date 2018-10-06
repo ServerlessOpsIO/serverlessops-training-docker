4fd85908613cfb1290e9d1fbe614c6705e4a2d3a
Tom McLaughlin <tom@serverlessops.io>
2018-10-06 13:15:44 -0400

    Set credetials.

--- files/startup.sh
+++ files/startup.sh
@@ -5,6 +5,15 @@ if [ -z "$SLS_STAGE" ]; then
     export SLS_STAGE=$sls_stage
 fi
 
+if [ -n "$AWS_ACCESS_KEY_ID" ]; then
+    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+    unset AWS_ACCESS_KEY_ID
+fi
+
+if [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
+    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+    unset AWS_SECRET_ACCESS_KEY
+fi
 
 aws configure
 
